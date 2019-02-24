57936cc685d00b0a695cda8f98589ba3949a7eba
sony <olama_18@hotmail.com>
2019-02-24 18:02:45 +0000

    clock file initiliased

---
+++ js/clock.js
@@ -0,0 +1,37 @@
+// wrapping clock in setInterval
+setInterval(() => {
+    let date = new Date();  
+  
+    // time variables
+    hr = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+    //date variable
+    let months = ['jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    let day = date.getDate();
+    let month = months[date.getUTCMonth()];
+    let year = date.getFullYear();
+
+
+    // DOM Cache
+    let hr_span = document.querySelector('#hr');
+    let min_span = document.querySelector('#min');
+    let sec_span = document.querySelector('#sec');
+    let date_span = document.querySelector('#date');
+
+    if (hr < 10) {
+        hr = '0' + hr;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+
+    hr_span.innerHTML = hr;
+    min_span.innerHTML = ':' + min;
+    sec_span.innerHTML = ':' + sec;
+    date_span.innerHTML = `${day} - ${month} - ${year}`;
+
+}, 1000);
